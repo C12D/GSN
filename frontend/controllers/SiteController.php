bfc2355ef98951bf836d3cbef3060bb15d80cd77
sonifir <sonifirdaus43@gmail.com>
2015-09-15 17:30:51 +0700

    15 september 2015

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -2,17 +2,21 @@
 namespace frontend\controllers;
 
 use Yii;
+use backend\models\Provinsi;
 use common\models\LoginForm;
 use frontend\models\PasswordResetRequestForm;
 use frontend\models\ResetPasswordForm;
 use frontend\models\SignupForm;
 use frontend\models\ContactForm;
 use yii\base\InvalidParamException;
+
 use yii\web\BadRequestHttpException;
 use yii\web\Controller;
 use yii\filters\VerbFilter;
 use yii\filters\AccessControl;
-
+use yii\helpers\ArrayHelper;
+use backend\models\Kota;
+use yii\helpers\Json;
 /**
  * Site controller
  */
@@ -29,11 +33,11 @@ class SiteController extends Controller
                 'rules' => [
                     [
                         /* Author: -ptr.nov- : Permission Allow No Login |index|error|login */
-                        'actions' => ['index', 'error','login'],
+                        'actions' => ['index', 'error','login','subcat','site'],
                         'allow' => true,
                     ],
                     [
-                        'actions' => ['logout', 'index'],
+                        'actions' => ['logout', 'index','subcat','site'],
                         'allow' => true,
                         'roles' => ['@'],
                     ],
@@ -66,7 +70,110 @@ class SiteController extends Controller
 
     public function actionIndex()
     {
-        return $this->render('index');
+       $model = new Provinsi();
+       $model1 = new Kota();
+
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->G_ORDER]);
+        } else {
+            return $this->render('index', [
+                'model' => $model,
+                'model1' => $model1,
+            ]);
+        }
+    }
+    public function actionLists($id)
+    {
+        $countPosts = Kota::find()
+                ->where(['province_id' => $id])
+                ->count();
+ 
+        $posts = Kota::find()
+                ->where(['province_id' => $id])
+                //->orderBy('id DESC')
+                ->all();
+ 
+        if($countPosts>0){
+            foreach($posts as $post){
+                echo "<option value='".$post->city_id."'>".$post->city_name."</option>";
+            }
+        }
+        else{
+            echo "<option>-</option>";
+        }
+ 
+    }
+          
+
+          public function actionSubcat() {
+    $out = [];
+    if (isset($_POST['depdrop_parents'])) {
+        $parents = $_POST['depdrop_parents'];
+        if ($parents != null) {
+            $cat_id = $parents[0];
+            $model = Kota::find()->asArray()->where(['province_id'=>$cat_id])->all();
+            //$out = self::getSubCatList($cat_id); 
+            // the getSubCatList function will query the database based on the
+            // cat_id and return an array like below:
+            // [
+            //    ['id'=>'<sub-cat-id-1>', 'name'=>'<sub-cat-name1>'],
+            //    ['id'=>'<sub-cat_id_2>', 'name'=>'<sub-cat-name2>']
+            // ]
+            foreach ($model as $key => $value) {
+                   $out[] = ['id'=>$value['province_id'],'name'=> $value['city_name']];
+               }
+ 
+               echo json_encode(['output'=>$out, 'selected'=>'']);
+               return;
+           }
+       }
+       echo Json::encode(['output'=>'', 'selected'=>'']);
+   }
+        public function actionSubcat2() {
+        $out = [];
+    if (isset($_POST['depdrop_parents'])) {
+        $parents = $_POST['depdrop_parents'];
+        if ($parents != null) {
+            $cat_id = $parents[0];
+            $out = self::getSubCatList($cat_id); 
+            // the getSubCatList function will query the database based on the
+            // cat_id and return an array like below:
+            // [
+            //    ['id'=>'<sub-cat-id-1>', 'name'=>'<sub-cat-name1>'],
+            //    ['id'=>'<sub-cat_id_2>', 'name'=>'<sub-cat-name2>']
+            // ]
+            echo Json::encode(['output'=>$out, 'selected'=>'']);
+            return;
+        }
+    }
+    echo Json::encode(['output'=>'', 'selected'=>'']);
+}
+    public function actionProd() {
+        $out = [];
+        if (isset($_POST['depdrop_parents'])) {
+            $ids = $_POST['depdrop_parents'];
+            print_r($ids);
+            $cat_id = empty($ids[0]) ? null : $ids[0];
+            if ($cat_id != null) {
+                $data = self::getProdList($cat_id);
+                /**
+                 * the getProdList function will query the database based on the
+                 * cat_id and sub_cat_id and return an array like below:
+                 *  [
+                 *      'out'=>[
+                 *          ['id'=>'<prod-id-1>', 'name'=>'<prod-name1>'],
+                 *          ['id'=>'<prod_id_2>', 'name'=>'<prod-name2>']
+                 *       ],
+                 *       'selected'=>'<prod-id-1>'
+                 *  ]
+                 */
+
+                echo Json::encode(['output'=>$data['out'], 'selected'=>$data['selected']]);
+                return;
+            }
+        }
+        echo Json::encode(['output'=>'', 'selected'=>'']);
     }
 
     public function actionLogin()
